62105486687a59a0ede2a80f78a6eb98c2e5bd08
frigidrain <frigid.rain@gmail.com>
2014-11-14 16:19:20 -0500

    Retabbed

--- greedy/11567.cpp
+++ greedy/11567.cpp
@@ -17,7 +17,7 @@
 #define sd(t) scanf("%f", &t)
 #define forr(i, a, b) for(int i=a; i<b;i++)
 #define foreach(it, container) \
-	for(typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+  for(typeof(container.begin()) it = container.begin(); it != container.end(); it++)
 #define MAX 1000
 
 using namespace std;
@@ -28,33 +28,33 @@ using namespace std;
 //vector<vii> adj;
 
 int countPowersOfTwo(int n) {
-	int i = 0;
-	while(n % 2 == 0) {
-		n = n/2;
-		i++;
-	}
-	return i;
+  int i = 0;
+  while(n % 2 == 0) {
+    n = n/2;
+    i++;
+  }
+  return i;
 }
 
 int main() {
-	
-	unsigned int n;
-	while(si(n) != EOF) {
-		int cnt = 0;
-		while(n > 0) {
-			if(n <= 3) {
-				cnt += n;
-				n = 0;
-				break;
-			} else if(n % 2 == 0) {
-				n = n/2;
-			} else if(countPowersOfTwo(n + 1) > countPowersOfTwo(n - 1)) {
-				n = n + 1;
-			} else {
-				n = n - 1;
-			}
-			cnt++;
-		}
-		cout << cnt << endl;
-	}
+  
+  unsigned int n;
+  while(si(n) != EOF) {
+    int cnt = 0;
+    while(n > 0) {
+      if(n <= 3) {
+        cnt += n;
+        n = 0;
+        break;
+      } else if(n % 2 == 0) {
+        n = n/2;
+      } else if(countPowersOfTwo(n + 1) > countPowersOfTwo(n - 1)) {
+        n = n + 1;
+      } else {
+        n = n - 1;
+      }
+      cnt++;
+    }
+    cout << cnt << endl;
+  }
 }
